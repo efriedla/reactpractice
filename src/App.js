75f2ad13b88a441f517cfbb3bf24eb4803e0cd32
efriedla <efriedla20852@gmail.com>
2017-11-30 07:50:45 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator/Calculator';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
